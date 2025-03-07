235021f660f1eb4d8de87a3069798ed8d966d5c3
FranInge <finge@myuax.com>
2025-03-07 09:44:16 +0100

    Creacion primer POO

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,45 +1,37 @@
-public class Alumno { //los identificadores (nombres) de las clases comienzan por  MAY y va
 
-    String DNI; //Atributos de instancia; son aquellos atributos de los que cada objeto va a tener un valor distinto
+public class Alumno { //Los identificadores (nombres) de las clases comienzan por MAY y van en singular
+    private String DNI;  //Atributos de instancia: son aquellos atributos de los que cada objeto va a tener un valor
+    private int NP;
+    private String nombreCompleto;
 
-    private
-    int NP;
-    String nombreCompleto;
-
-    public Alumno(){ //Metodo constructor por defecto, si no se indica nada, se asignan valores por defecto
-
+    public Alumno(){ //Metodo constructor por defecto, si no se indica nada, se asignan valores por defecto Si no se indica nada, se asignan valores por defecto
         DNI="00.000.000-E";
         NP=0;
         nombreCompleto="Nombre Apellido1 Apellido2";
-
     }
 
-    public  Alumno(String DNIp, int NPp, String nc){
-
-        char primerDigito = DNI.charAt(0); // De la cadena DNIp dame le caracter que esta en la posicion 0
-
-        if( !Character.isDigit(primerDigito) ) { // !Character.isDigit Si no se cumple esta hacer tal cosa
-            throw new IllegalArgumentException("DNI incorrecto"); //Se pone arriba throws Exception para que no de error
+    public Alumno(String DNIp, int NPp, String nc ){
+        char primerDigito= DNIp.charAt(0);
+        if ( !Character.isDigit(primerDigito)){
+            throw new IllegalArgumentException("DNI incorrecto");
         }
-
-        if (NPp <= 0) {
+        if (NPp<0){
             throw new IllegalArgumentException("NP incorrecto");
         }
-        DNI = DNIp;
-        NP = NPp;
-        nombreCompleto = nc;
+        DNI= DNIp;
+        NP= NPp;
+        nombreCompleto= nc;
     }
 
     public String getDNI() {
         return DNI;
     }
 
-    public void setDNI(String DNI) { //Se agrega lo del IllegalArgumentException para al escribir un nombre raro, no lo acepte
-        char primerDigito = DNI.charAt(0);
-        if( !Character.isDigit(primerDigito) ) {
+    public void setDNI(String DNI) {
+        char primerDigito= DNI.charAt(0);
+        if ( !Character.isDigit(primerDigito)){
             throw new IllegalArgumentException("DNI incorrecto");
         }
-
         this.DNI = DNI;
     }
 
@@ -48,10 +40,9 @@ public class Alumno { //los identificadores (nombres) de las clases comienzan po
     }
 
     public void setNP(int NP) {
-        if (NP <= 0) {
-            throw new IllegalArgumentException("NP incorrecto");
+        if (NP<0){
+            throw new IllegalArgumentException("DNI incorrecto");
         }
-
         this.NP = NP;
     }
 
@@ -62,6 +53,8 @@ public class Alumno { //los identificadores (nombres) de las clases comienzan po
     public void setNombreCompleto(String nombreCompleto) {
         this.nombreCompleto = nombreCompleto;
     }
+
+
 
     @Override
     public String toString() {
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,71 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Alumno al= new Alumno();
-        Alumno al2 = new Alumno("00", -123, "asdasdasda");
+        Alumno al= new Alumno(); //Cuando no se pone nada, se va a escribir el constructor por defecto que escribimos en la clase Alumno
+        //Alumno al2 = new Alumno("00", -123, "davo"); Va a dar error porque el NP es menor a 0
 
+        Alumno al2 = new Alumno("00.000.001-E", 123, "asdasdasda");
+
+        Alumno al3 = al2; //Va a copiar todos los datos guardados en al2 y va a tener los mismo datos
+
+        Alumno al4 = new Alumno("00.000.001-E", 123, "asdasdasda"); //Una nueva referencia y objeto, pero con los mismo datos que al2 y su direccion de memoria es distinta
         System.out.println("al: " + al.toString()); //"00 ... 0" 0 "nombre      "
-        System.out.println("al2: " + al2.toString()); //"11" 123 "adasdasdasd   "
+        System.out.println("al2: " + al2.toString()); //"00 ... 1-E" 123 "adasdasdasd   "
+        System.out.println("al3: " + al3.toString()); //"00 ... 1-E" 123 "adasdasdasd   "
+        System.out.println("al4: " + al4.toString()); //"00 ... 1-E" 123 "adasdasdasd   "
+
+        //al3.DNI="00.000.002-E"; //No se puede cambiar el DNI porque esta en private el valor del atributo
+
+        al3.setDNI("00.000.002-E"); //De esta forma si se puede cambiar el valor de DNI
+        //al4.setDNI("A0.000.002-E"); //Va a dar error porque el primer digito no es un numero
+        //al4.setNP(-3); No es posible asignar un NP menor a 0
+
+        int a=19;
+        int b=a;
+        if (a==b){
+            System.out.println("a y b son enteros que tienen el mismo valor");
+        }
+
+        if(al==al2){
+            System.out.println("al y al2 son referencias que tienen el mismo valor");
+        } else {
+            System.out.println("al y al2 son referencias que no tienen el mismo valor");
+        }
+
+        if(al2==al3){
+            System.out.println("al2 y al3 son referencias que tienen el mismo valor");
+        } else {
+            System.out.println("al2 y al3 son referencias que no tienen el mismo valor");
+        }
+
+        if(al2==al4){
+            System.out.println("al2 y al4 son referencias que tienen el mismo valor");
+        } else {
+            System.out.println("al2 y al4 son referencias que no tienen el mismo valor, porque no tienen la misma direccion de memoria");
+        }
+
+        if(al2.equals (al4)){
+            System.out.println("al2 y al4 son objetos que se cosnideran iguales porque tienen el mismo DNI");
+        } else {
+            System.out.println("al2 y al4 son objetos que se NO cosnideran iguales porque NO tienen el mismo DNI");
+        }
+
+        if(al2.hashCode() == al4.hashCode()){
+            System.out.println("al2 y al4 son objetos que se tienen el mismo hashCode");//Tienen la misma longitud
+        } else {
+            System.out.println("al2 y al4 son objetos que se NO tienen el mismo hashCode");
+        }
+
+        if(al.equals (al4)){
+            System.out.println("al y al4 son objetos que se cosnideran iguales porque tienen el mismo DNI");
+        } else {
+            System.out.println("al y al4 son objetos que se NO cosnideran iguales porque NO tienen el mismo DNI");
+        }
+
+        if(al.hashCode() == al4.hashCode()){
+            System.out.println("al y al4 son objetos que se tienen el mismo hashCode");//Tienen la misma longitud
+        } else {
+            System.out.println("al y al4 son objetos que se NO tienen el mismo hashCode");
+        }
     }
 }
